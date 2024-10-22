484c65a04fe1b4283cab206b2a53ee12d2e04f61
KamoD-a <70842126+KamoD-a@users.noreply.github.com>
2024-10-22 17:59:30 +0200

    added buttons to floatingobjects

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FloatingElements/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FloatingElements from './components/FloatingElements.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FloatingElements
   }
 }
 </script>
@@ -18,9 +17,6 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/FloatingElements.vue
@@ -0,0 +1,85 @@
+<template>
+    <body>
+    <div class="main-container">
+        <div class="animated_objects">
+            <div class="animation-slow">
+                <img src="../assets/cloud_pink.png">
+            </div>
+            <div class="animation-wiggle">
+                <img src="../assets/wiggle_yellow.png">
+            </div>
+            <div class="animation-slow">
+                <img src="../assets/cloud_pink.png">
+            </div>
+            <div class="animation-slow">
+                <img src="../assets/cloud_pink.png">
+            </div>
+        </div>
+    </div>
+    </body>
+</template>
+
+<script setup>
+</script>
+
+<style>
+body{
+    overflow: hidden;
+    background-image: url("../assets/background.png");
+    background: cover;
+    background-size: cover;
+    background-repeat: no-repeat;
+    padding: -10px;
+    background-attachment: fixed;
+}
+
+
+
+.animation-slow img{
+  position: absolute;
+  animation: moves 20s infinite linear;
+}
+
+.animation{
+    display: flex;
+}
+
+@keyframes moves {
+    0% {
+      top: -100%;
+      left: -100%;
+   }
+   50% {
+      top: 50%;
+      left: 50%;
+      transform: rotate( 40deg );
+   }
+
+   100% {
+      top: 100%;
+      left: 100%;
+      transform: rotate(-40deg );
+   }
+}
+
+.animation-wiggle img{
+  position: absolute;
+  animation: wiggle 15s infinite ease-in-out;
+}
+
+.animation{
+    display: flex;
+}
+
+@keyframes wiggle {
+    from {
+      top: -100%;
+      left: -100%;
+   }
+   to {
+      top: 50%;
+      left: 100%;
+      transform: rotate( 360deg );
+   }
+}
+</style> 
